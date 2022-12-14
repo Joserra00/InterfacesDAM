4882657d155d42c49c90e329807c9dd105fb3637
Joserra00 <joseramongarrido175@gmail.com>
2022-12-14 08:27:19 +0100

    nueva actu

--- gitpull.sh
+++ gitpull.sh
@@ -3,3 +3,4 @@ git pull https://github.com/Joserra00/InterfacesDAM.git master
 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
 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
 8dtm-gbzh-gtge
+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
